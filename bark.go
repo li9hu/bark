f043aa26e9dcc4038c6d8c6bbcd4a3cc4bf8c16a
ewanLi <ewan9hu@gmail.com>
2024-03-12 16:11:59 +0800

    [新增] UUID() 返回消息唯一ID

--- bark.go
+++ bark.go
@@ -38,10 +38,35 @@ func SetUp(key, url string) {
 	bark = b
 }
 
-// Run 请求Bark 返回消息唯一ID
-func Run(group, title, data, sound string) (string, error) {
+// UUID 返回消息唯一ID
+func UUID(group, title, data, sound string) (string, error) {
 	if bark == nil {
 		return "", fmt.Errorf("please use bark.SetUp first")
+	}
+	// 构造 bark 请求
+	body := map[string]string{
+		"title":      title,
+		"body":       data,
+		"device_key": bark.Key,
+		"group":      group,
+		"sound":      sound,
+	}
+	b, err := json.Marshal(body)
+	if err != nil {
+		return "", err
+	}
+	// 生成唯一ID
+	hasher := md5.New()
+	hasher.Write(b)
+	hashBytes := hasher.Sum(nil)
+	// 将哈希值转换为固定长度的字符串ID
+	id := hex.EncodeToString(hashBytes)
+	return id, nil
+}
+
+func Run(group, title, data, sound string) error {
+	if bark == nil {
+		return fmt.Errorf("please use bark.SetUp first")
 	}
 
 	// 构造 bark 请求
@@ -58,29 +83,22 @@ func Run(group, title, data, sound string) (string, error) {
 	}
 	b, err := json.Marshal(body)
 	if err != nil {
-		return "", fmt.Errorf("bark request body to json: %v", err)
+		return fmt.Errorf("bark request body to json: %v", err)
 	}
-
-	// 生成唯一ID
-	hasher := md5.New()
-	hasher.Write(b)
-	hashBytes := hasher.Sum(nil)
-	// 将哈希值转换为固定长度的字符串ID
-	id := hex.EncodeToString(hashBytes)
 
 	// 尝试发送 Bark
 	ret := rehttp.Post(url, head, b)
 	if ret.Err != nil {
-		return "", fmt.Errorf("bark post: %v", ret.Err)
+		return fmt.Errorf("bark post: %v", ret.Err)
 	}
 	barkResult := &Result{}
 	err = json.Unmarshal([]byte(ret.ResponseBody), barkResult)
 	if err != nil {
-		return "", fmt.Errorf("bark result to json: %v", err)
+		return fmt.Errorf("bark result to json: %v", err)
 	}
 
 	if barkResult.Code != 200 {
-		return "", fmt.Errorf("bark status != 200: %s", barkResult.Message)
+		return fmt.Errorf("bark status != 200: %s", barkResult.Message)
 	}
-	return id, nil
+	return nil
 }
